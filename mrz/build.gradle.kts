7a2fc2e82d7657b90ddeca7f3ff128671491b64d
Bastien Paul <bastien.paul@coppernic.fr>
2021-10-12 16:19:30 +0200

    Add publication script

--- mrz/build.gradle.kts
+++ mrz/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
+    val kotlinVersion = "1.5.31"
+
     id("net.nemerosa.versioning") version "2.14.0"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     id("jacoco")
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.serialization") version "1.5.31"
-    id("org.jetbrains.dokka") version "1.5.31"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+    id("org.jetbrains.dokka") version kotlinVersion
 }
 
 apply(from = rootProject.file("gradle/ktlint.gradle"))
@@ -37,3 +39,5 @@ dependencies {
     testImplementation("org.amshove.kluent:kluent:1.65")
     implementation("ch.qos.logback:logback-classic:1.2.6")
 }
+
+apply(from = rootProject.file("gradle/gradle-publish.gradle"))
